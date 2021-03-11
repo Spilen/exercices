63b915f9a0664367b3d51fc38adf71c23756927b
Spilen <Mika@MacBook-Pro-de-Demo.local>
2021-03-11 18:31:24 +0100

    Muhlbo WIP

---
+++ muhlbo/js/home.js
@@ -0,0 +1,16 @@
+const signs = document.querySelectorAll('.album-themes')
+const randomIn = (min, max) => (
+  Math.floor(Math.random() * (max - min + 1) + min)
+)
+
+const mixupInterval = el => {
+  const ms = randomIn(2000, 4000)
+  el.style.setProperty('--interval', `${ms}ms`)
+}
+
+signs.forEach(el => {
+  mixupInterval(el)
+  el.addEventListener('webkitAnimationIteration', () => {
+    mixupInterval(el)
+  })
+})
