7b0c654d54513e3790132f25e6108f2b35f452bf
Spilen <Mika@MacBook-Pro-de-Demo.local>
2021-07-14 20:11:12 +0200

    bug vuejs

---
+++ exercice-8/js/app.js
@@ -0,0 +1,25 @@
+// scroll-top
+
+let vm = new Vue ({
+  el: '#return-on-top',
+
+  methods: {
+    scrollTop: function () {
+      this.intervalId = setInterval(() => {
+        if (window.pageYOffset === 0) {
+          clearInterval(this.intervalId)
+        }
+        window.scroll(0, window.pageYOffset - 50)
+      }, 20)
+    },
+    scrollListener: function (e) {
+      this.visible = window.scrollY > 150
+    }
+  },
+  mounted: function () {
+    window.addEventListener('scroll', this.scrollListener)
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('scroll', this.scrollListener)
+  }
+})
