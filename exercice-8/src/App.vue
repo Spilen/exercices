e078c6fea5c98de2242a4ba8401f66e86003f4fa
Spilen <Mika@MacBook-Pro-de-Demo.local>
2021-07-16 17:38:30 +0200

    installation Webpack + carousel.vue WIP

---
+++ exercice-8/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <carousel>
+      <carousel-slide>
+        1
+      </carousel-slide>
+      <carousel-slide>
+        2
+      </carousel-slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+
+  import carousel from './components/carousel/carousel.vue'
+  import carouselSlide from './components/carousel/carouselSlide.vue'
+
+  export default {
+    components: {
+      carousel,
+      carouselSlide
+    }
+
+  }
+  
+</script>
